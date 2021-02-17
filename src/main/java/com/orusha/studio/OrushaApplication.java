199d7bf94822f7705c681c92e76a724b2b815e5a
Mohamed Toure <demahom.toure@gmail.com>
2021-02-17 22:04:53 +0100

    first commit

---
+++ src/main/java/com/orusha/studio/OrushaApplication.java
@@ -0,0 +1,13 @@
+package com.orusha.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrushaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrushaApplication.class, args);
+	}
+
+}
